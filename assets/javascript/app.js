29a0a81eacfe9a3f8ce2b4e062bfe0b3b1a667b6
Jacob Tate <jacob87608@gmail.com>
2020-01-31 10:14:44 -0600

    added a onclick function to add trains to the table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
 
+$("#button").on("click", function(event){
+event.preventDefault();
+var trainName = $(".input1").val();
+var dest = $(".input2").val();
+var trainTime = $(".input3").val();
+var Frequency = $(".input4").val();
+
+console.log(trainName, dest, trainTime, Frequency);
+var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(dest),
+    $("<td>").text(trainTime),
+    $("<td>").text(Frequency)
+)
+$(".table").append(newRow)
+
+});
 
 
-
-    
 });
