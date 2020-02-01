b4ffedfb7cf0c51bef04eb5c960c83e70c12b145
Jacob Tate <jacob87608@gmail.com>
2020-01-31 20:44:56 -0600

    added firebase to the js and trim functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,27 @@
 $(document).ready(function(){
+    //firebase
+     // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCr9t9QA1Wr1q6Jmkt9YMGy4Tz3V1yKfDk",
+    authDomain: "train-scheduler-a1ead.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a1ead.firebaseio.com",
+    projectId: "train-scheduler-a1ead",
+    storageBucket: "train-scheduler-a1ead.appspot.com",
+    messagingSenderId: "110599949226",
+    appId: "1:110599949226:web:a1a68bdc28167dbb6639d3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
 
 $("#button").on("click", function(event){
 event.preventDefault();
-var trainName = $(".input1").val();
-var dest = $(".input2").val();
-var trainTime = $(".input3").val();
-var Frequency = $(".input4").val();
+var trainName = $(".input1").val().trim();
+var dest = $(".input2").val().trim();
+var trainTime = $(".input3").val().trim();
+var Frequency = $(".input4").val().trim();
 
 console.log(trainName, dest, trainTime, Frequency);
 var newRow = $("<tr>").append(
@@ -15,6 +31,18 @@ var newRow = $("<tr>").append(
     $("<td>").text(Frequency)
 )
 $(".table").append(newRow)
+
+database.ref().push({
+Name: trainTime,
+Dest: dest,
+Time: trainTime,
+Frequency: Frequency
+})
+
+ $(".input1").val("")
+$(".input2").val("")
+ $(".input3").val("")
+$(".input4").val("")
 
 });
 
